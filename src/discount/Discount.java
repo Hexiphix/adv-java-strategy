82eea1b5202e00d5845797c49345036737779d37
Hexiphix <jmolenda2002@gmail.com>
2022-05-04 18:44:05 -0500

    Changed or Created for Module 6 - Quiz

--- src/discount/Discount.java
+++ src/discount/Discount.java
@@ -2,8 +2,6 @@ package discount;
 
 public class Discount implements DiscountStyle{
     public double applyDiscount(double billAmount) {
-        double discountedBill;
-        discountedBill = billAmount - (billAmount * 0.5);
-        return discountedBill;
+        return billAmount - (billAmount * 0.5);
     }
 }
--- src/discount/Liquidation.java
+++ src/discount/Liquidation.java
@@ -2,8 +2,6 @@ package discount;
 
 public class Liquidation implements DiscountStyle{
     public double applyDiscount(double billAmount) {
-        double discountedBill;
-        discountedBill = billAmount - (billAmount * 0.75);
-        return discountedBill;
+        return billAmount - (billAmount * 0.75);
     }
 }
